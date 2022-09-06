68ef74263a38a8fdec0239c169e94808b7d38d34
Nova <48789639+Nova-Static@users.noreply.github.com>
2022-09-06 10:11:08 +0200

    build

--- dist/multiselect.js
+++ dist/multiselect.js
@@ -2471,7 +2471,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     }, [
       renderSlot(_ctx.$slots, "beforelist", { options: _ctx.fo }),
       createElementVNode("ul", {
-        class: normalizeClass(_ctx.classList.options),
+        class: normalizeClass([_ctx.classList.options , "listsimplebar"]),
         style: {"max-height":"15rem"},
         id: _ctx.ariaOwns,
         role: "listbox"
@@ -2533,7 +2533,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
             }), 128 /* KEYED_FRAGMENT */))
           : (openBlock(), createBlock(_component_SimpleBar, {
               key: 1,
-              class: "listsimplebar px-0 py-0 w-full h-full relative overflow-y-auto inline-block pr-4"
+              class: "px-0 py-0 w-full h-full relative overflow-y-auto inline-block pr-4"
             }, {
               default: withCtx(() => [
                 (openBlock(true), createElementBlock(Fragment, null, renderList(_ctx.fo, (option, i, key) => {
